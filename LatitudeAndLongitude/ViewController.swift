24c3d0c8b7b8e16ea738742ee0be82fc29937184
Dora <domonshieh@gmail.com>
2021-09-15 00:34:35 +0800

    TextField 拉出完成

--- LatitudeAndLongitude/ViewController.swift
+++ LatitudeAndLongitude/ViewController.swift
@@ -1,17 +1,28 @@
-//
-//  ViewController.swift
-//  LatitudeAndLongitude
-//
-//  Created by 謝孟芩 on 2021/9/13.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+//經緯度十進位TextField
+    @IBOutlet weak var DDLatitude1: UITextField!
+    @IBOutlet weak var DDLongitude1: UITextField!
+   //經緯度度分(DMM)TextField
+    @IBOutlet weak var DMMLatitude1: UITextField!
+    @IBOutlet weak var DMMLatitude2: UITextField!
+    @IBOutlet weak var DMMLongitude1: UITextField!
+    @IBOutlet weak var DMMLongitude2: UITextField!
+    //經緯度度分(DMS)TextField
+    @IBOutlet weak var DMSLatitude1: UITextField!
+    @IBOutlet weak var DMSLatitude2: UITextField!
+    @IBOutlet weak var DMSLatitude3: UITextField!
+    @IBOutlet weak var DMSLongitude1: UITextField!
+    @IBOutlet weak var DMSLongitude2: UITextField!
+    @IBOutlet weak var DMSLongitude3: UITextField!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 
