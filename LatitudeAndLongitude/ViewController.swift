1849c6df570e8b9990c052cbdbd747a7d06b9672
Dora <domonshieh@gmail.com>
2021-09-15 19:29:07 +0800

    DD轉換按鈕完成

--- LatitudeAndLongitude/ViewController.swift
+++ LatitudeAndLongitude/ViewController.swift
@@ -24,7 +24,59 @@ class ViewController: UIViewController
         super.viewDidLoad()
        
     }
-
-
+   //DD轉換按鈕
+    @IBAction func ConvertDD(_ sender: UIButton)
+    {   //收鍵盤
+        view.endEditing(true)
+        
+        //緯度
+        //當使用者沒輸入完點轉換就帶入預設值
+        //空字串
+        let DDLatitudetext = Double(String(DDLatitude1.text ?? ""))
+        let DDLongitudetext = Double(String(DDLongitude1.text ?? ""))
+        //0.0
+        let DDLat1 = Int(DDLatitudetext ?? 0.0)
+        let DDLon1 = Int(DDLongitudetext ?? 0.0)
+        
+        DMMLatitude1.text = "\(DDLat1)"
+        DMSLatitude1.text = "\(DDLat1)"
+        DMMLongitude1.text = "\(DDLon1)"
+        DMSLongitude1.text = "\(DDLon1)"
+        
+        //緯分
+        let DMMLat1 = ((DDLatitudetext ?? 0.0) - Double(DDLat1) * 60)
+        let DMMLat1Int = Int(DMMLat1)
+        let DMMLon1 = ((DDLongitudetext ?? 0.0) - Double(DDLon1) * 60)
+        let DMMLon1Int = Int(DMMLon1)
+        
+        //%.4f取小數點四位
+        DMMLatitude2.text = String(format: "%.4f", DMMLat1)
+        DMSLatitude2.text = "\(DMMLat1Int)"
+        DMMLongitude2.text = String(format: "%.4f", DMMLon1)
+        DMSLongitude2.text = "\(DMMLon1Int)"
+        
+        //緯秒
+        let DMSLat1 = (DMMLat1 - Double(DMMLat1Int)) * 60
+        let DMSLon1 = (DMMLon1 - Double(DMMLon1Int)) * 60
+        //%.2f取小數點二位
+        DMSLatitude3.text = String(format: "%.2f", DMSLat1)
+        DMSLongitude3.text = String(format: "%.2f", DMSLon1)
+        
+    }
+    
+    
+    //DMM轉換按鈕
+    @IBAction func ConvertDMM(_ sender: UIButton)
+    {
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
 }
 
